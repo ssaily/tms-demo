8ad08ad38dbdf1a4e30e7657bab04254f96fa04a
Sami Saily <samisaily@Aiven108.int.saily.fi>
2020-12-03 17:07:09 +0200

    Added PostgreSql, Kafka Connect cluster and CDC connector

--- infra/kafka_services.tf
+++ infra/kafka_services.tf
@@ -18,4 +18,51 @@ resource "aiven_kafka" "tms-demo-kafka" {
   }
 }
 
+// Kafka connect service
+resource "aiven_kafka_connect" "tms-demo-kafka-connect1" {
+  project = var.avn_project_id
+  cloud_name = var.cloud_name
+  plan = "startup-4"
+  service_name = "tms-demo-kafka-connect1"
+  maintenance_window_dow = "monday"
+  maintenance_window_time = "10:00:00"
 
+  kafka_connect_user_config {
+    kafka_connect {
+      consumer_isolation_level = "read_committed"
+    }
+
+    public_access {
+      kafka_connect = true
+    }
+  }
+}
+
+// Kafka connect service integration
+resource "aiven_service_integration" "tms-demo-connect-integr" {
+  project = var.avn_project_id
+  integration_type = "kafka_connect"
+  source_service_name = aiven_kafka.tms-demo-kafka.service_name
+  destination_service_name = aiven_kafka_connect.tms-demo-kafka-connect1.service_name
+
+  kafka_connect_user_config {
+    kafka_connect {
+      group_id = "connect_1"
+      status_storage_topic = "__connect_1_status"
+      offset_storage_topic = "__connect_1_offsets"
+      config_storage_topic = "__connect_1_configs"
+    }
+  }
+}
+
+data "aiven_service_user" "kafka_admin" {
+  project = var.avn_project_id
+  service_name = aiven_kafka.tms-demo-kafka.service_name
+
+  # default admin user that is automatically created each Aiven service
+  username = "avnadmin"
+
+  depends_on = [
+    aiven_kafka.tms-demo-kafka
+  ]
+}
