523ec117fba71d23774b6f94eccac6c5c6235172
Sami Saily <23719929+ssaily@users.noreply.github.com>
2020-12-22 11:55:47 +0200

    add spark example

--- spark/src/main/scala/SparkExampleS3.scala
+++ spark/src/main/scala/SparkExampleS3.scala
@@ -10,24 +10,7 @@ import org.apache.log4j.{LogManager, Level}
 import org.apache.spark.sql.expressions.Window
 
 object SparkExample {
-    val log = LogManager.getLogger(SparkExample.getClass())
-    
-    val registryConfig = Map(
-        AbrisConfig.SCHEMA_REGISTRY_URL -> "https://tms-demo-kafka-sa-demo.aivencloud.com:24952",
-        "basic.auth.credentials.source" -> "USER_INFO",
-        "basic.auth.user.info" -> "avnadmin:msee1ipnpe8ch39u"
-        )
-
-    val abrisConfig = AbrisConfig
-    .fromConfluentAvro
-    .downloadReaderSchemaByLatestVersion
-    .andTopicNameStrategy("observations.weather.municipality", isKey=false)
-    .usingSchemaRegistry(registryConfig) // use the map instead of just url
-
-    import za.co.absa.abris.avro.functions.from_avro    
-    def readAvro(dataFrame: DataFrame, fromAvroConfig: FromAvroConfig): DataFrame = {
-       dataFrame.select(from_avro(col("value"), fromAvroConfig) as 'data).select("data.*")
-    }
+    val log = LogManager.getLogger(SparkExample.getClass())    
 
     def main(args: Array[String]) {        
         log.info("Starting")
@@ -39,12 +22,9 @@ object SparkExample {
         import spark.implicits._
         val w = Window.orderBy(col("measuredTime")).rangeBetween(-3600000, Window.currentRow)
 
-        park.read.format("avro").load("s3a://tmsdemo/topics/observations.weather.municipality/processdate=20201221/")
+        val df = spark.read.format("avro").load("s3a://tmsdemo/topics/observations.weather.municipality/processdate=20201221/")
                     
-        val deserialized = readAvro(df, abrisConfig)
-        log.info(deserialized.schema.toString())       
-
-        val byRange = deserialized
+        val byRange = df
             .filter(col("id") === 1)
             .filter(col("roadStationId") === 7031)
             .withColumn("Date", to_timestamp($"measuredTime" / 1000))
