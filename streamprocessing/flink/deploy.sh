8399921d12427b06bb7194e6546d19659cb2e619
Sami Säily <23719929+ssaily@users.noreply.github.com>
2023-04-03 09:44:14 +0300

    Fixed Flink application deployment script

--- streamprocessing/flink/deploy.sh
+++ streamprocessing/flink/deploy.sh
@@ -5,16 +5,22 @@ export INTEGRATION_ID=$(avn service integration-list tms-demo-kafka --project $1
 
 if [ -z "INTEGRATION_ID" ]; then
     echo "Flink Kafka integration missing. Please check Terraform output!"
-    exit
+    exit 1
 fi
 
 # check if we have an application already
 APPLICATION_ID=$(avn service flink list-applications tms-demo-flink --project $1 |jq -r '.applications[] | select(.name == "weather") | .id')
 if [ -z "$APPLICATION_ID" ]; then
     echo "Create new application"
-    APPLICATION_ID==$(avn service flink create-application tms-demo-flink --project $1 @application.json | jq -r '.id')
+    APPLICATION_ID=$(avn service flink create-application tms-demo-flink --project $1 @application.json | jq -r '.id')
 else
     echo "Use application $APPLICATION_ID"
+    # Check if we have running deployment for this Application
+    DEPLOYMENT_ID=$(avn service flink list-application-deployments tms-demo-flink --project $1 --application-id $APPLICATION_ID|jq -r '.deployments[] | select(.status == "RUNNING") | .id')
+    if [ ! -z "$DEPLOYMENT_ID" ]; then
+        echo "Stopping running deployment ${DEPLOYMENT_ID}"
+        avn service flink stop-application-deployment tms-demo-flink --project $1 --application-id $APPLICATION_ID --deployment-id $DEPLOYMENT_ID
+    fi
 fi
 
 if [ "$unamestr" = 'Linux' ]; then
@@ -35,20 +41,17 @@ APP_JSON=$(jq -r --arg source_table "$SOURCE_TABLE" --arg sink_table "$SINK_TABL
 '.sources[].create_table |= $source_table | .sinks[].create_table |= $sink_table | .statement |= $statement' \
 topology.template.json | envsubst | jq -Rsa .)
 
+echo "Create new application version"
 export VERSION_ID=$(avn service flink create-application-version tms-demo-flink --project $1 --application-id $APPLICATION_ID """$APP_JSON"""|jq -r '.id')
 if [ -z "$VERSION_ID" ]; then
     echo "Failed to deploy application version!"
-    exit
-fi
-
-# Check if we have running deployment for this Application
-DEPLOYMENT_ID=$(avn service flink list-application-deployments tms-demo-flink --project $1 --application-id 3a276971-cbe9-47c6-9ba5-2a7e6d0f0de1|jq -r '.deployments[] | select(.status == "RUNNING") | .id')
-
-if [ ! -z "$DEPLOYMENT_ID" ]; then
-    echo "Stop running deployment"
-    avn service flink stop-application-deployment tms-demo-flink --project $1 --application-id $APPLICATION_ID --deployment-id $DEPLOYMENT_ID
+    exit 1
+else
+    echo "Applicatation version deployed succesfully"
 fi
 
 VERSION_JSON=$(envsubst < deployment.template.json | jq -Rsa .)
+echo "Creating new application deployment"
 avn service flink create-application-deployment tms-demo-flink --project $1 --application-id $APPLICATION_ID """$VERSION_JSON"""
 echo "Application deployed succesfully!"
+exit 0
