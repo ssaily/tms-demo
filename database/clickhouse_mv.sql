e48ad7f357ec4d37ab5749225175ba6a40c93cf4
Sami Säily <23719929+ssaily@users.noreply.github.com>
2023-08-17 13:11:29 +0300

    Fix measuredTime field to include milliseconds ( DateTime64(3) )

--- database/clickhouse_mv.sql
+++ database/clickhouse_mv.sql
@@ -1,4 +1,4 @@
-create table weather.observations_ob (roadStationId LowCardinality(String), sensorId UInt64, sensorName String, sensorValue Float, sensorUnit String, geohash String, measuredTime DateTime CODEC(Delta(4), ZSTD(1))) ENGINE=ReplicatedMergeTree ORDER BY (roadStationId, sensorId, measuredTime);
+create table weather.observations_ob (roadStationId LowCardinality(String), sensorId UInt64, sensorName String, sensorValue Float, sensorUnit String, geohash String, measuredTime DateTime64(3) CODEC(Delta(4), ZSTD(1))) ENGINE=ReplicatedMergeTree ORDER BY (roadStationId, sensorId, measuredTime);
 CREATE MATERIALIZED VIEW weather.observations_ob_mv TO weather.observations_ob AS
 SELECT
     _key AS roadStationId,
