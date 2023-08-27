d6cb56633a28b4c540c39e86e2faccccec9fe765
Sami Säily <23719929+ssaily@users.noreply.github.com>
2023-08-27 18:27:39 +0300

    Fix delimiter error while importing traffic stations

--- database/import-stations.sh
+++ database/import-stations.sh
@@ -4,9 +4,9 @@ curl -X GET "https://tie.digitraffic.fi/api/weather/v1/stations" -H "accept: app
 
 psql "$(< pgpassfile)" -c "\copy weather_stations (roadstationid, name, latitude, longitude) from 'weather_stations.csv' CSV DELIMITER ',';"
 
-curl -X GET "https://tie.digitraffic.fi/api/tms/v1/stations" -H "accept: application/geo+json" -H "Accept-Encoding: gzip, deflate"|jq -r '.features|map([(.properties.id|tostring), .properties.name, (.geometry.coordinates[1]|tostring), (.geometry.coordinates[0]|tostring)]|join(","))|join("\n")' > traffic_stations.csv
+curl -X GET "https://tie.digitraffic.fi/api/tms/v1/stations" -H "accept: application/geo+json" -H "Accept-Encoding: gzip, deflate"|jq -r '.features|map([(.properties.id|tostring), .properties.name, (.geometry.coordinates[1]|tostring), (.geometry.coordinates[0]|tostring)]|join("\t"))|join("\n")' > traffic_stations.csv
 
-psql "$(< pgpassfile)" -c "\copy traffic_stations (roadstationid, name, latitude, longitude) from 'traffic_stations.csv' CSV DELIMITER ',';"
+psql "$(< pgpassfile)" -c "\copy traffic_stations (roadstationid, name, latitude, longitude) from 'traffic_stations.csv' CSV DELIMITER E'\t';"
 
 curl -X GET "https://tie.digitraffic.fi/api/weather/v1/sensors" -H "accept: application/json" -H "Accept-Encoding: gzip, deflate"|gzip -dc|jq -r '.sensors|map([(.id|tostring), .name, .unit, (.accuracy|tostring)]|join(","))|join("\n")' > weather_sensors.csv
 
