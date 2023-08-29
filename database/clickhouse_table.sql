5b3e464c61346f4926361bc832446737c153c021
Sami Säily <23719929+ssaily@users.noreply.github.com>
2023-08-29 15:22:57 +0300

    Optimise CH schema. Add compression.

--- database/clickhouse_table.sql
+++ database/clickhouse_table.sql
@@ -2,11 +2,12 @@ create table weather.observations_ob (
         roadStationId UInt16,
         sensorId UInt8,
         sensorName LowCardinality(String),
-        sensorValue Float,
+        sensorValue Float32 CODEC(Gorilla, ZSTD(1)),
         sensorUnit LowCardinality(String),
         geohash String,
-        h3_10 UInt64,
-        measuredTime DateTime64(3) CODEC(Delta(4), ZSTD(1))
+        h3_10 UInt64 Codec(T64, ZSTD(1)),
+        measuredTime DateTime64(3) CODEC(DoubleDelta, ZSTD(1))
     )
     ENGINE=ReplicatedReplacingMergeTree
+    PARTITION BY (toYYYYMM(measuredTime))
     ORDER BY (roadStationId, sensorId, measuredTime);
